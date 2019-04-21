782054b19d3a7f5ea2bee5c057d7a54741ff9c44
Andres Chia <andres_chia11@hotmail.com>
2019-04-21 02:13:57 -0500

    COMMIT: cambios en home

--- src/app/Components/home/footer-home/FooterHomeComponent.ts
+++ src/app/Components/home/footer-home/FooterHomeComponent.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from "../../../app.component";
+
 @Component({
     selector: 'app-footer-home',
     templateUrl: './footer-home.component.html',
