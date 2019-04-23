02900fc38c2457c6bbde4e4f02e70ab7caa55a96
Andres Chia <andres_chia11@hotmail.com>
2019-04-22 22:37:51 -0500

    COMMIT: arreglo produccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,22 +11,21 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
-import { FooterHomeComponent } from './Components/home/footer-home/FooterHomeComponent';
 import { EspecificacionComponent } from './Components/especificacion/especificacion.component';
 import { NavBarComponent } from './Components/home/nav-bar/nav-bar.component';
 import { FondoHomeComponent } from './Components/home/fondo-home/fondo-home.component';
 import { PotencialComponent } from './Components/potencial/potencial.component';
-
+import { FooterHomeComponent } from "./Components/home/footer-home/footer-home.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FooterHomeComponent,
     EspecificacionComponent,
     NavBarComponent,
     FondoHomeComponent,
-    PotencialComponent
+    PotencialComponent,
+    FooterHomeComponent
   ],
   imports: [
     HttpClientModule,
